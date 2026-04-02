a11d156e9f2718b924bc31132864996a5b11d765
Raphaela Monteiro <raphaela.monteiro@waveconcept.com.br>
2026-04-02 14:47:59 -0300

    fat: ajustando tela inicial do agente

--- app/(agent)/page.tsx
+++ app/(agent)/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>teste</h1>;
+  return (
+    <div className="min-h-screen flex flex-col bg-[var(--white-base)] px-4 py-6 items-start text-left">
+      <h1 className="title-1 mb-2">Bem-vindo!</h1>
+      <p className="text-base max-w-4xl">
+        Visualização dos chamados abertos, de acordo com grupo e nível de suporte que você foi cadastrado.
+      </p>
+    </div>
+  );
 }
