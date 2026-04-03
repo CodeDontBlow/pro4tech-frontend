bb47f6381b42f98c1debb45a9b2b41ea9fc5f773
Victor Godoy <victor.hgodoy@icloud.com>
2026-04-03 12:41:55 -0300

    fix: proxy.ts regras atualizadas

--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function proxy(req: NextRequest) {
+export function proxy(req: NextRequest) { 
     const token = req.cookies.get("token")?.value;
+    const userRole = req.cookies.get("user_role")?.value;
     const pathname = req.nextUrl.pathname;
 
-    // --- ROTAS DO AGENT ---
-    const isAgentRoute = 
-        pathname.startsWith("/dashboard") || 
-        pathname.startsWith("/history") || 
-        pathname.startsWith("/profile") || 
-        pathname.startsWith("/tickets");
-
-    // --- ROTAS DO ADMIN ---
     const isAdminRoute = 
         pathname.startsWith("/admin-dashboard") || 
         pathname.startsWith("/admin-profile") || 
@@ -20,19 +13,44 @@ export function proxy(req: NextRequest) {
         pathname.startsWith("/manage-companies") || 
         pathname.startsWith("/screening");
 
-    // --- LOGICA DE PROTEÇÃO ---
-    if (isAgentRoute && !token) {
+    const isAgentRoute = 
+        pathname.startsWith("/dashboard") || 
+        pathname.startsWith("/history") || 
+        pathname.startsWith("/profile") || 
+        pathname.startsWith("/tickets");
+
+    // 1. Verificação token 
+    if ((isAdminRoute || isAgentRoute) && !token) {
         return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    // 2. Bloqueia AGENT de entrar em rota ADMIN
+    if (isAdminRoute && userRole !== "ADMIN") {
+        return NextResponse.redirect(new URL("/profile", req.url));
+    }
+
+    // 3. Bloqueia ADMIN de entrar em rota AGENT    
+    if (isAgentRoute && userRole === "ADMIN") {
+        return NextResponse.redirect(new URL("/admin-profile", req.url));
     }
 
     return NextResponse.next();
 }
 
+
 export const config = {
     matcher: [
+        // Agent rotas
         "/dashboard/:path*",
         "/history/:path*",
         "/profile/:path*",
         "/tickets/:path*",
+
+        // Admin rotas
+        "/admin-dashboard/:path*",
+        "/admin-profile/:path*",
+        "/manage-agents/:path*",
+        "/manage-companies/:path*",
+        "/screening/:path*",
     ],
 };
