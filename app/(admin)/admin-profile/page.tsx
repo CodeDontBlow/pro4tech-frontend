148502f5760bb6ee8a27e42c372c2f509ad3344f
Raphaela <bmraphaela@gmail.com>
2026-05-28 21:25:03 -0300

    feat: atualizando página do admin para o dashboard

--- app/(admin)/admin-profile/page.tsx
+++ app/(admin)/admin-profile/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
-    <h1 className="flex justify-center text-blue-600 text-4xl">
-      Profile ADMIN
-    </h1>
-  );
+    <div className="py-10 px-15">
+      <header className="mb-10">
+        <h1 className="title-2 text-center text-[var(--teal-700)]">
+          Bem-vindo!
+        </h1>
+      </header>
+    </div>
+  )
 }
