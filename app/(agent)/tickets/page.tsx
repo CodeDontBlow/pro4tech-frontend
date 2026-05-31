f9f5c9e4e50769d1ecd16bb129d1624b72242820
Raphaela <bmraphaela@gmail.com>
2026-05-31 16:00:42 -0300

    fix: ajustando redirecionamento após o login

--- app/(agent)/tickets/page.tsx
+++ app/(agent)/tickets/page.tsx
@@ -1,16 +1,20 @@
-"use client"    
+"use client";
+
 import useTicket from "./hooks/useTicket"
 import GroupTable from "./components/groupTable"
 
 export default function Page() {
-    const { tickets, groups, loading, fetchTickets } = useTicket()
+    const { tickets, groups, loading, fetchTickets } = useTicket();
+
 
     return (
         <div className="py-10 px-15">
             <header className="mb-10">
-                <h1 className="title-2 text-left" onClick={() => {console.log(tickets, groups)}}>
-                    Chamados
+
+                <h1 className="title-2 text-left">
+                    Bem Vindo!
                 </h1>
+
                 <p className="text-2 text-left">
                     Visualização dos chamados abertos, de acordo com grupo e nível de suporte que você foi cadastrado.
                 </p>
@@ -18,7 +22,7 @@ export default function Page() {
 
             <section className="flex flex-col gap-10">
                 {groups.map((group) => (
-                    <GroupTable 
+                    <GroupTable
                         group={group}
                         tickets={tickets.filter((ticket) => ticket.supportGroup.id === group.id)}
                         description="Clique em um dos chamados abertos do seu grupo de atendimento abaixo para atribuí-lo a você."
@@ -27,7 +31,7 @@ export default function Page() {
                     />
                 ))}
             </section>
-        
+
         </div>
     );
 }
